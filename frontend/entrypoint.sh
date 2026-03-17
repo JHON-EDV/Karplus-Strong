e9ba39ea1e82157ac853f4aa3cf7a3aa2c629388
Jhon E Vargas <jhonrm1997@gmail.com>
2026-03-16 21:40:37 -0500

    add port to front

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -6,4 +6,8 @@ if [ -n "$BACKEND_URL" ]; then
     sed -i "s|BACKEND_URL_PLACEHOLDER|${BACKEND_URL}|g" /usr/share/nginx/html/js/config.js
 fi
 
+# Substitute ${PORT} in nginx config template (Railway sets PORT dynamically)
+export PORT="${PORT:-3000}"
+envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
 exec "$@"
